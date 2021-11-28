e84ea357ab0a7b4c6ab92e5ed22da8d270081eff
M Shashank <94677981+Shashank1726@users.noreply.github.com>
2021-11-28 14:30:03 +0000

    Coding

--- p0original.c
+++ p0original.c
@@ -1,1 +1,9 @@
- 
+#include<stdio.h>
+int main()
+{
+  char a[50];
+  printf("enter your name\n");
+  scanf("%s",a);
+  printf("%s",a);
+  return 0;
+}
---
+++ p5original.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() 
+{
+   int a, b, c;
+
+   a = 21;
+   b = 27;
+   c = 33;
+
+   if ( a > b && a > c )
+      printf("%d is the largest.", a);
+   else if ( b > a && b > c )
+      printf("%d is the largest.", b);
+   else if ( c > a && c > b )
+      printf("%d is the largest.", c);
+   else   
+      printf("Values are not unique");
+
+   return 0;
+}
