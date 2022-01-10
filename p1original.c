19647ef0d9ad125aadb28fff4fb74dd44b574219
M Shashank <94677981+Shashank1726@users.noreply.github.com>
2022-01-10 05:47:27 +0000

    Revision Final

--- p1original.c
+++ p1original.c
@@ -6,7 +6,6 @@ int main()
    scanf("%d", &num1);
    printf("Enter second number: ");
    scanf("%d", &num2);
-
    sum = num1 + num2;
    printf("Sum of the entered numbers: %d", sum);
    return 0;
